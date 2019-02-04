bf181cb485b50365ec2361aea73de7151a4c45cb
Luis Ramos <lramos@gyssa.com>
2019-02-04 15:46:45 -0600

    Se elimina identificador de ejecucion de las stage.

--- 09-IMPRESION DE EXTRACTOS-SFC/09-IMPRESION DE EXTRACTOS-SFC-STG_SAT_SFCIMPEXT_BON.sql
+++ 09-IMPRESION DE EXTRACTOS-SFC/09-IMPRESION DE EXTRACTOS-SFC-STG_SAT_SFCIMPEXT_BON.sql
@@ -1,5 +1,5 @@
 drop table IF EXISTS STG_SAT.SFCIMPEXT_BON;  CREATE TABLE IF NOT EXISTS STG_SAT.SFCIMPEXT_BON  ( 
-,	TIPOREGI	VARCHAR	(1)	--Identifica el tipo de registro para que sea tratado por los procesos. 
+	TIPOREGI	VARCHAR	(1)	--Identifica el tipo de registro para que sea tratado por los procesos. 
 --En este caso llevará el valor fijo D. (C=Cabecera y D=Detalle)	 	 		
 ,	CODENTID	VARCHAR	(8)	--Código de Entidad. Identifica la entidad fidelizadora que ha generado las acciones del Evento. 
 ,	CODCINTA	VARCHAR	(2)	--Código de Cinta.  Código reservado que identifica la cinta relacionada con el proceso que la trata o genera.

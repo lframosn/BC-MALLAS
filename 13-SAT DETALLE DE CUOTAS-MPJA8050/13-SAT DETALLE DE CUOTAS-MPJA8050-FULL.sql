90ad9ba9d3b107ffd6e7f02c95ffc32d8b075829
Luis Ramos <lramos@gyssa.com>
2019-02-07 13:25:44 -0600

    Se corrige nombre de la malla en todos los archivos

--- 13-SAT DETALLE DE CUOTAS-MPJA8050/13-SAT DETALLE DE CUOTAS-MPJA8050-FULL.sql
+++ 13-SAT DETALLE DE CUOTAS-MPJA8050/13-SAT DETALLE DE CUOTAS-MPJA8050-FULL.sql
@@ -14,81 +14,65 @@ BEGIN
 END;
 COMMIT;
 
-drop table IF EXISTS SAT.TBHISMOD;  CREATE TABLE IF NOT EXISTS SAT.TBHISMOD  ( 	IDENTIFICADOR_EJECUCION	INTEGER		--IDENTIFICADOR UNICO DE EJECUCION
---MPDT068-Histórico de Modificaciones	 	 		
-,	CODENT_ENC	VARCHAR	(4)	--Código de entidad
-,	OFICINA	VARCHAR	(4)	--Oficina Usuario
-,	USUARIO	VARCHAR	(8)	--Usuario que realiza la acción
-,	TIPO_ACCION	VARCHAR	(30)	--Acción realizada por el usuario
-,	TIPO_REGISTRO	VARCHAR	(1)	--REGISTRO DETALLE (alfanumerico 2300)
---1 - Log Movimientos formato  ATLOGMOV	 	 		
---2 – Histórico Modificaciones formato ATHISMOD	 	 		
---DETALLE DE COLUMNAS ATHISMOD	 	 		
+drop table IF EXISTS SAT.TBDETCUO;  CREATE TABLE IF NOT EXISTS SAT.TBDETCUO  ( 	IDENTIFICADOR_EJECUCION	INTEGER		--IDENTIFICADOR UNICO DE EJECUCION
+--MPDT207-Detalle de cuotas	 	 		
 ,	CODENT 	VARCHAR	(4)	--Código de entidad 
 ,	CENTALTA 	VARCHAR	(4)	--Centro de alta cuenta de tarjeta 
 ,	CUENTA 	VARCHAR	(12)	--Cuenta de tarjeta 
-,	NUMSECHIS 	DECIMAL	(15,0)	--Número secuencial del histórico 
-,	PAN 	VARCHAR	(22)	--PAN de la tarjeta. El valor de este campo deberá almacenarse justificado a la izquierda 
-,	CODCOM 	VARCHAR	(15)	--Código de comercio 
-,	FECHAMOD 	DATE		--Fecha de modificación 
-,	HORAMOD 	TIME		--Hora de modificación 
-,	TIPACCES 	DECIMAL	(1,0)	--Tipo de acceso: 
---0  – Cuenta de tarjeta 	 	 		
---1  – Tarjeta 	 	 		
---2  – Comercio 	 	 		
-,	PROCESO 	VARCHAR	(10)	--Nombre del proceso que realizó la modificación 
-,	DESPROCESO 	VARCHAR	(30)	--Descripción del proceso o función 
-,	DATOMOD 	VARCHAR	(2)	--Dato modificado (bancos, bloqueos, …) 
-,	MOTIVO 	VARCHAR	(2)	--Motivo de la modificación 
-,	CANALMOD 	VARCHAR	(2)	--Canal de la modificación (en persona, teléfono …) 
+,	CLAMON 	DECIMAL	(3,0)	--Código de moneda 
+,	NUMOPECUO 	DECIMAL	(6,0)	--Número de operación en compra en cuotas 
+,	NUMFINAN 	DECIMAL	(3,0)	--Número de financiación 
+,	NUMCUOTA 	DECIMAL	(9,0)	--Número de cuota 
+,	FECPROCUO 	DATE		--Fecha de cobro de la cuota 
+,	IMPCUOTA 	DECIMAL	(17,2)	--Importe de la cuota 
+,	IMPCAPITAL 	DECIMAL	(17,2)	--Importe capital 
+,	IMPINTERESES 	DECIMAL	(17,2)	--Importe interés 
+,	IMPIMPTO 	DECIMAL	(17,2)	--Importe del impuesto 
+,	ESTCUO 	DECIMAL	(2,0)	--Estado de la cuota: 
+--01  – Pendiente 	 	 		
+--02  – Liquidada 	 	 		
+--03  – Cancelada 	 	 		
+--04  – Vencida 	 	 		
+--05  – Anticipada	 	 		
+,	FECLIQ 	DATE		--Fecha liquidación 
+,	FECCONTA 	DATE		--Fecha contable 
+,	ESTCONT 	DECIMAL	(1,0)	--Estado contable. 
 ,	CODENTUMO 	VARCHAR	(4)	--Entidad última modificación                           
 ,	CODOFIUMO 	VARCHAR	(4)	--Oficina última modificación                           
 ,	USUARIOUMO 	VARCHAR	(8)	--Usuario última modificación                           
 ,	CODTERMUMO 	VARCHAR	(8)	--Terminal última modificación                          
 ,	CONTCUR 	TIMESTAMP		--Control de concurrencia                               
-,	TABLA 	VARCHAR	(3)	--Nombre de tabla modificada 
-,	REGANT 	VARCHAR	(1024)	--Registro anterior 
-,	REGACT 	VARCHAR	(1024)	--Registro actual 
-,	FILLER1	VARCHAR	(62)	--FILLER
 ,	MIS_DATE	DATE		--Fecha de generación de informacion
 ); COMMIT;	 	 		
 
-drop table IF EXISTS STG_SAT.TBHISMOD;  CREATE TABLE IF NOT EXISTS STG_SAT.TBHISMOD  (
---MPDT068-Histórico de Modificaciones	 	 		
-	CODENT_ENC	VARCHAR	(4)	--Código de entidad
-,	OFICINA	VARCHAR	(4)	--Oficina Usuario
-,	USUARIO	VARCHAR	(8)	--Usuario que realiza la acción
-,	TIPO_ACCION	VARCHAR	(30)	--Acción realizada por el usuario
-,	TIPO_REGISTRO	VARCHAR	(1)	--REGISTRO DETALLE (alfanumerico 2300)
---1 - Log Movimientos formato  ATLOGMOV	 	 		
---2 – Histórico Modificaciones formato ATHISMOD	 	 		
---DETALLE DE COLUMNAS ATHISMOD	 	 		
-,	CODENT 	VARCHAR	(4)	--Código de entidad 
+drop table IF EXISTS STG_SAT.TBDETCUO;  CREATE TABLE IF NOT EXISTS STG_SAT.TBDETCUO  (
+--MPDT207-Detalle de cuotas	 	 		
+	CODENT 	VARCHAR	(4)	--Código de entidad 
 ,	CENTALTA 	VARCHAR	(4)	--Centro de alta cuenta de tarjeta 
 ,	CUENTA 	VARCHAR	(12)	--Cuenta de tarjeta 
-,	NUMSECHIS 	DECIMAL	(15,0)	--Número secuencial del histórico 
-,	PAN 	VARCHAR	(22)	--PAN de la tarjeta. El valor de este campo deberá almacenarse justificado a la izquierda 
-,	CODCOM 	VARCHAR	(15)	--Código de comercio 
-,	FECHAMOD 	DATE		--Fecha de modificación 
-,	HORAMOD 	TIME		--Hora de modificación 
-,	TIPACCES 	DECIMAL	(1,0)	--Tipo de acceso: 
---0  – Cuenta de tarjeta 	 	 		
---1  – Tarjeta 	 	 		
---2  – Comercio 	 	 		
-,	PROCESO 	VARCHAR	(10)	--Nombre del proceso que realizó la modificación 
-,	DESPROCESO 	VARCHAR	(30)	--Descripción del proceso o función 
-,	DATOMOD 	VARCHAR	(2)	--Dato modificado (bancos, bloqueos, …) 
-,	MOTIVO 	VARCHAR	(2)	--Motivo de la modificación 
-,	CANALMOD 	VARCHAR	(2)	--Canal de la modificación (en persona, teléfono …) 
+,	CLAMON 	DECIMAL	(3,0)	--Código de moneda 
+,	NUMOPECUO 	DECIMAL	(6,0)	--Número de operación en compra en cuotas 
+,	NUMFINAN 	DECIMAL	(3,0)	--Número de financiación 
+,	NUMCUOTA 	DECIMAL	(9,0)	--Número de cuota 
+,	FECPROCUO 	DATE		--Fecha de cobro de la cuota 
+,	IMPCUOTA 	DECIMAL	(17,2)	--Importe de la cuota 
+,	IMPCAPITAL 	DECIMAL	(17,2)	--Importe capital 
+,	IMPINTERESES 	DECIMAL	(17,2)	--Importe interés 
+,	IMPIMPTO 	DECIMAL	(17,2)	--Importe del impuesto 
+,	ESTCUO 	DECIMAL	(2,0)	--Estado de la cuota: 
+--01  – Pendiente 	 	 		
+--02  – Liquidada 	 	 		
+--03  – Cancelada 	 	 		
+--04  – Vencida 	 	 		
+--05  – Anticipada	 	 		
+,	FECLIQ 	DATE		--Fecha liquidación 
+,	FECCONTA 	DATE		--Fecha contable 
+,	ESTCONT 	DECIMAL	(1,0)	--Estado contable. 
 ,	CODENTUMO 	VARCHAR	(4)	--Entidad última modificación                           
 ,	CODOFIUMO 	VARCHAR	(4)	--Oficina última modificación                           
 ,	USUARIOUMO 	VARCHAR	(8)	--Usuario última modificación                           
 ,	CODTERMUMO 	VARCHAR	(8)	--Terminal última modificación                          
 ,	CONTCUR 	TIMESTAMP		--Control de concurrencia                               
-,	TABLA 	VARCHAR	(3)	--Nombre de tabla modificada 
-,	REGANT 	VARCHAR	(1024)	--Registro anterior 
-,	REGACT 	VARCHAR	(1024)	--Registro actual 
-,	FILLER1	VARCHAR	(62)	--FILLER
 ); COMMIT;	 	 		
 
 ------------------------------------------------
@@ -117,8 +101,8 @@ declare
 ,@CRDATE DATETIME
 
 select 
- @NOMBRE_MALLA = 'N/A.SAT-TABLAS VARIAS'
-,@OBJECT_NAME = 'TBHISMOD'
+ @NOMBRE_MALLA = 'MPJA8050'
+,@OBJECT_NAME = 'TBDETCUO'
 ,@OBJECT_USER = 'SAT'
 ,@VERSION_COMMIT = '1ed3eb78899734e14d597abf39d2c6dfb77470d0'
 ,@VERSION_CODE = '1ed3eb7'
@@ -142,8 +126,8 @@ select
  @NOMBRE_MALLA, @OBJECT_NAME ,@OBJECT_USER ,@VERSION_COMMIT,@VERSION_CODE ,@VERSION_DATE ,@CRDATE 
 
 select 
- @NOMBRE_MALLA = 'N/A.SAT-TABLAS VARIAS'
-,@OBJECT_NAME = 'TBHISMOD'
+ @NOMBRE_MALLA = 'MPJA8050'
+,@OBJECT_NAME = 'TBDETCUO'
 ,@OBJECT_USER = 'STG_SAT'
 ,@VERSION_COMMIT = '1ed3eb78899734e14d597abf39d2c6dfb77470d0'
 ,@VERSION_CODE = '1ed3eb7'

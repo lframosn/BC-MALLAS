bcc5f2107862d345617c6c81cfb6ba8465b2c681
Luis Ramos <lramos@gyssa.com>
2019-03-12 14:57:24 -0600

    Version inicial TBDETCUO-PPJDW07N

---
+++ 15-SAT DETALLE DE CUOTAS FINANCIADAS-PPJDW07N/15-SAT DETALLE DE CUOTAS FINANCIADAS-PPJDW07N-FULL.sql
@@ -0,0 +1,158 @@
+--- creacion de grupo de usuarios SAT
+IF NOT EXISTS (SELECT 1 FROM DBO.SYSUSERS WHERE name = 'SAT')
+BEGIN
+  CREATE USER SAT 
+  FORCE PASSWORD CHANGE OFF;
+END;
+COMMIT;
+
+--- creacion de grupo de usuarios STG_SAT
+IF NOT EXISTS (SELECT 1 FROM DBO.SYSUSERS WHERE name = 'STG_SAT')
+BEGIN
+  CREATE USER STG_SAT
+  FORCE PASSWORD CHANGE OFF;
+END;
+COMMIT;
+
+drop table IF EXISTS SAT.TBDETCUO;  CREATE TABLE IF NOT EXISTS SAT.TBDETCUO  ( 	IDENTIFICADOR_EJECUCION	INTEGER		--IDENTIFICADOR UNICO DE EJECUCION
+--MPDT207-Detalle de cuotas	 	 		
+,	CODENT_D	VARCHAR	(4)	--CODIGO DE ENTIDAD
+,	NSECFIC_D	DECIMAL	(10,0)	--NUMERO DE SECUENCIA DE LA CINTA
+,	TIPOCINTA_D	DECIMAL	(2,0)	--TIPO DE LA CINTA
+,	TIPOREG_D	VARCHAR	(1)	--TIPO DE REGISTRO D = DETALLE 
+,	FILLER_D	VARCHAR	(91)	--FILLER
+,	NUMREGPROC	DECIMAL	(12,0)	--NUMERO DE REGISTRO PROCESADO
+,	CODENT 	VARCHAR	(4)	--Código de entidad 
+,	CENTALTA 	VARCHAR	(4)	--Centro de alta cuenta de tarjeta 
+,	CUENTA 	VARCHAR	(12)	--Cuenta de tarjeta 
+,	CLAMON 	DECIMAL	(3,0)	--Código de moneda 
+,	NUMOPECUO 	DECIMAL	(6,0)	--Número de operación en compra en cuotas 
+,	NUMFINAN 	DECIMAL	(3,0)	--Número de financiación 
+,	NUMCUOTA 	DECIMAL	(9,0)	--Número de cuota 
+,	FECPROCUO 	DATE		--Fecha de cobro de la cuota 
+,	IMPCUOTA 	DECIMAL	(17,2)	--Importe de la cuota 
+,	IMPCAPITAL 	DECIMAL	(17,2)	--Importe capital 
+,	IMPINTERESES 	DECIMAL	(17,2)	--Importe interés 
+,	IMPIMPTO 	DECIMAL	(17,2)	--Importe del impuesto 
+,	ESTCUO 	DECIMAL	(2,0)	--Estado de la cuota: 
+--01  - Pendiente 	 	 		
+--02  - Liquidada 	 	 		
+--03  - Cancelada 	 	 		
+--04  - Vencida 	 	 		
+--05  - Anticipada	 	 		
+,	FECLIQ 	DATE		--Fecha liquidación 
+,	FECCONTA 	DATE		--Fecha contable 
+,	ESTCONT 	DECIMAL	(1,0)	--Estado contable. 
+,	FILLER	VARCHAR	(738)	--Filler
+); COMMIT;	 	 		
+
+drop table IF EXISTS STG_SAT.TBDETCUO;  CREATE TABLE IF NOT EXISTS STG_SAT.TBDETCUO  (
+--MPDT207-Detalle de cuotas	 	 		
+ 	CODENT_D	VARCHAR	(4)	--CODIGO DE ENTIDAD
+,	NSECFIC_D	DECIMAL	(10,0)	--NUMERO DE SECUENCIA DE LA CINTA
+,	TIPOCINTA_D	DECIMAL	(2,0)	--TIPO DE LA CINTA
+,	TIPOREG_D	VARCHAR	(1)	--TIPO DE REGISTRO D = DETALLE 
+,	FILLER_D	VARCHAR	(91)	--FILLER
+,	NUMREGPROC	DECIMAL	(12,0)	--NUMERO DE REGISTRO PROCESADO
+,	CODENT 	VARCHAR	(4)	--Código de entidad 
+,	CENTALTA 	VARCHAR	(4)	--Centro de alta cuenta de tarjeta 
+,	CUENTA 	VARCHAR	(12)	--Cuenta de tarjeta 
+,	CLAMON 	DECIMAL	(3,0)	--Código de moneda 
+,	NUMOPECUO 	DECIMAL	(6,0)	--Número de operación en compra en cuotas 
+,	NUMFINAN 	DECIMAL	(3,0)	--Número de financiación 
+,	NUMCUOTA 	DECIMAL	(9,0)	--Número de cuota 
+,	FECPROCUO 	DATE		--Fecha de cobro de la cuota 
+,	IMPCUOTA 	DECIMAL	(17,2)	--Importe de la cuota 
+,	IMPCAPITAL 	DECIMAL	(17,2)	--Importe capital 
+,	IMPINTERESES 	DECIMAL	(17,2)	--Importe interés 
+,	IMPIMPTO 	DECIMAL	(17,2)	--Importe del impuesto 
+,	ESTCUO 	DECIMAL	(2,0)	--Estado de la cuota: 
+--01  - Pendiente 	 	 		
+--02  - Liquidada 	 	 		
+--03  - Cancelada 	 	 		
+--04  - Vencida 	 	 		
+--05  - Anticipada	 	 		
+,	FECLIQ 	DATE		--Fecha liquidación 
+,	FECCONTA 	DATE		--Fecha contable 
+,	ESTCONT 	DECIMAL	(1,0)	--Estado contable. 
+,	FILLER	VARCHAR	(738)	--Filler
+); COMMIT;	 	 		
+
+------------------------------------------------
+---- CONTROL DE VERSION ------------------------
+------------------------------------------------
+create table IF NOT EXISTS CNF.VERSION_OBJETOS (
+ NOMBRE_MALLA varchar(100)
+,OBJECT_NAME varchar(100)
+,OBJECT_USER varchar(20)
+,VERSION_COMMIT VARCHAR(100)
+,VERSION_CODE VARCHAR(20)
+,VERSION_DATE DATETIME
+,CRDATE DATETIME null default getdate()
+,CRUSER varchar(20) null default user_name()
+)
+;
+COMMIT;
+
+declare 
+ @NOMBRE_MALLA varchar(100)
+,@OBJECT_NAME varchar(100)
+,@OBJECT_USER varchar(20)
+,@VERSION_COMMIT VARCHAR(100)
+,@VERSION_CODE VARCHAR(20)
+,@VERSION_DATE DATETIME 
+,@CRDATE DATETIME
+
+select 
+ @NOMBRE_MALLA = 'PPJDW07N'
+,@OBJECT_NAME = 'TBDETCUO'
+,@OBJECT_USER = 'SAT'
+,@VERSION_COMMIT = '3265bef4429c7a87c524f52143b1fc7e04d065be'
+,@VERSION_CODE = '3265bef'
+,@VERSION_DATE = 'March 12, 2019 2:55:32 PM'
+,@CRDATE = NULL
+
+SELECT @CRDATE = create_time 
+FROM SYSUSERS U, SYSTABLE T, SYSIQTABLE S
+where T.table_name = @OBJECT_NAME
+  and T.creator = U.uid
+  and U.name = @OBJECT_USER
+  and T.table_id = S.table_id
+if @CRDATE IS NULL SELECT @CRDATE = getdate()
+
+IF EXISTS(SELECT 1 from CNF.VERSION_OBJETOS where OBJECT_NAME = @OBJECT_NAME AND OBJECT_USER = @OBJECT_USER AND VERSION_CODE = @VERSION_CODE)
+  DELETE CNF.VERSION_OBJETOS where OBJECT_NAME = @OBJECT_NAME AND OBJECT_USER = @OBJECT_USER AND VERSION_CODE = @VERSION_CODE
+insert CNF.VERSION_OBJETOS (
+ NOMBRE_MALLA, OBJECT_NAME ,OBJECT_USER ,VERSION_COMMIT ,VERSION_CODE ,VERSION_DATE ,CRDATE
+)
+select 
+ @NOMBRE_MALLA, @OBJECT_NAME ,@OBJECT_USER ,@VERSION_COMMIT,@VERSION_CODE ,@VERSION_DATE ,@CRDATE 
+
+select 
+ @OBJECT_USER = 'STG_SAT'
+,@CRDATE = NULL
+
+SELECT @CRDATE = create_time 
+FROM SYSUSERS U, SYSTABLE T, SYSIQTABLE S
+where T.table_name = @OBJECT_NAME
+  and T.creator = U.uid
+  and U.name = @OBJECT_USER
+  and T.table_id = S.table_id
+if @CRDATE IS NULL SELECT @CRDATE = getdate()
+
+IF EXISTS(SELECT 1 from CNF.VERSION_OBJETOS where OBJECT_NAME = @OBJECT_NAME AND OBJECT_USER = @OBJECT_USER AND VERSION_CODE = @VERSION_CODE)
+  DELETE CNF.VERSION_OBJETOS where OBJECT_NAME = @OBJECT_NAME AND OBJECT_USER = @OBJECT_USER AND VERSION_CODE = @VERSION_CODE
+insert CNF.VERSION_OBJETOS (
+ NOMBRE_MALLA, OBJECT_NAME ,OBJECT_USER ,VERSION_COMMIT ,VERSION_CODE ,VERSION_DATE ,CRDATE
+)
+select 
+ @NOMBRE_MALLA, @OBJECT_NAME ,@OBJECT_USER ,@VERSION_COMMIT,@VERSION_CODE ,@VERSION_DATE ,@CRDATE 
+
+COMMIT;
+
+-- Inserts NUEVAS MALLAS
+IF NOT EXISTS(SELECT 1 FROM "CNF"."MALLAS" WHERE "NOMBRE_MALLA" = 'PPJDW07N' )
+BEGIN
+  INSERT INTO "CNF"."MALLAS" ("NOMBRE_MALLA","DESCRIPCION","ENCABEZADO","CONTROL_FECHA") VALUES('PPJDW07N','Detalle de Cuotas Financiadas',1,0);
+END
+COMMIT;

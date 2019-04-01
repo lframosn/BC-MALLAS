e4153e83d2ba7be7cbb3e64187d53365ac8d5d86
Luis Ramos <lramos@gyssa.com>
2019-04-01 13:56:17 -0600

    Se corrige el comentario

--- TABLAS INTERIN/02-BUCKET MORA/02-BUCKET_MORA-INDICES.sql
+++ TABLAS INTERIN/02-BUCKET MORA/02-BUCKET_MORA-INDICES.sql
@@ -1,5 +1,5 @@
 -----------------------------
--INDICES ASOCIADOS A LA VISTA
+--INDICES ASOCIADOS A LA VISTA
 -----------------------------
 create HG index if not exists idx_HG_PCENVPO1_RI_CUENTA on SAT.PCENVPO1_RI(CUENTA)
 create HG index if not exists idx_HG_PCENVPO1_RI_FECVENMOV on SAT.PCENVPO1_RI(FECVENMOV)

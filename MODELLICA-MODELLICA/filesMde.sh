1b98b99805141226d1d924117aceb7a528f89ea9
Luis Ramos <lramos@gyssa.com>
2019-04-04 10:24:49 -0600

    Version inicial

---
+++ MODELLICA-MODELLICA/filesMde.sh
@@ -0,0 +1,15 @@
+##/********************************************************           
+##  * filesMde.sh		                                   *   
+##  *                                                      *   
+##  * Author:  Mario Valdez | Gyssa                      *   
+##  *                                                      *   
+##  * Purpose:  Generar un archivo listando todos los      * 
+##              archivo cuyo nombre incluya .xml        *   
+##  *                                                      *   
+##  * Usage:                     *   
+##  *      JOB_LOAD_FILESNAMES		                   *   
+##  ********************************************************/ 
+
+
+cd /datos/SAT/mde/
+  ls -I listadoArchivos.txt -I All.xml *.xml > listadoArchivosXML.txt
